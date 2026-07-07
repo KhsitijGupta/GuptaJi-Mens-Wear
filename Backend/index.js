47fdc210b81853e1dc4c2db8dc639afe67918b1d
Kshitij Gupta <kshitijgupta2162@gmail.com>
2026-07-07 12:48:23 +0530

    corrected the image paths

--- Backend/index.js
+++ Backend/index.js
@@ -53,7 +53,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("//uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use("/api/admins", adminRoutes);
